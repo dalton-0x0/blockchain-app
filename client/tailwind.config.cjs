f5a9e712f8f1881bd9d63c9889d53caaeecb08e7
Dalton Okechukwu <dalton.0x0@gmail.com>
2022-09-11 09:19:29 -0600

    blockchain: updates packages and config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  mode: "jit",
+  darkMode: "media", // 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
     },
-    plugins: [],
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
